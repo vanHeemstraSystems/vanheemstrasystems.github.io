b7e1945c27efc1cda33f67b4c485c67cb29d4f60
wvanheemstra <willem@vanheemstrasystems.com>
2018-02-19 22:52:23 +0100

    Update

--- step003/calculator/javascript/src/Main.purs
+++ step003/calculator/javascript/src/Main.purs
@@ -1,9 +1,15 @@
 module Main where
 
 import Prelude
+import Data.Array (drop)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Node.Process (PROCESS, argv)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+message :: forall t1. t1 -> t1
+message s = s
+
+main :: forall e. Eff (console :: CONSOLE, process :: PROCESS | e) Unit
 main = do
-  log "Hello World!"
+    args <- argv
+    log $ show (message (drop 2 args))
